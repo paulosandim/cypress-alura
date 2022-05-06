03124d749ac78791900bbead0011b9d39726a516
paulo sandim <pauloedusandim@gmail.com>
2022-05-05 22:20:43 -0300

    searching java course

---
+++ cypress/integration/1-getting-started/alura-busca.spec.js
@@ -0,0 +1,14 @@
+describe('alura busca cursos', () => {
+
+    beforeEach(() => {
+        cy.visit('https://www.alura.com.br');
+    })
+
+    it('buscar curso de java', () => {
+        cy.get('#header-barraBusca-form-campoBusca').type('java');
+        cy.get('.header-barraBusca-form-submit').click();
+        cy.get(':nth-child(3) > .busca-resultado-link > .busca-resultado-container > .busca-resultado-nome')
+            .should('have.text', 'Formação Java e Orientação a Objetos');
+    })
+
+})
