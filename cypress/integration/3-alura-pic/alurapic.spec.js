5ed4c07fba6e49b353f265e98ef6e93bb2973ec0
paulo sandim <pauloedusandim@gmail.com>
2022-05-05 23:43:43 -0300

    verification alternative scenario

--- cypress/integration/3-alura-pic/alurapic.spec.js
+++ cypress/integration/3-alura-pic/alurapic.spec.js
@@ -13,5 +13,20 @@ describe('Login e registro de usuários alura pic', () => {
         cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
         cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
     })
-    
+
+    it('verifica mensagens de email invalido', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="email"]').type('paulo');
+        cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible');
+    })
+
+    it('verifica mensagens de senha com menos de 8 caracteres', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="password"]').type('123');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
+    })
+
 })
