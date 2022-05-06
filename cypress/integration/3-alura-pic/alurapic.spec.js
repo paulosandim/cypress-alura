897db38193b96c5bb6397c0e68752100c07e5041
paulo sandim <pauloedusandim@gmail.com>
2022-05-05 23:09:05 -0300

    start alura pic

---
+++ cypress/integration/3-alura-pic/alurapic.spec.js
@@ -0,0 +1,17 @@
+describe('Login e registro de usuários alura pic', () => {
+
+    beforeEach(() => {
+        cy.visit('https://alura-fotos.herokuapp.com')
+    })
+
+    it('verifica mensagens validacao', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Email is required!').should('be.visible');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Full name is required!').should('be.visible');
+        cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
+        cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
+    })
+    
+})
