c0b6100ed84caa37aebf3b2c8848439ee80165cc
paulo sandim <pauloedusandim@gmail.com>
2022-05-05 23:56:09 -0300

    add more alternative scenarios

--- cypress/integration/3-alura-pic/alurapic.spec.js
+++ cypress/integration/3-alura-pic/alurapic.spec.js
@@ -14,19 +14,44 @@ describe('Login e registro de usuários alura pic', () => {
         cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
     })
 
-    it('verifica mensagens de email invalido', () => {
+    it('verifica mensagem de email invalido', () => {
         cy.contains('a', 'Register now').click();
         cy.contains('button', 'Register').click();
         cy.get('input[formcontrolname="email"]').type('paulo');
         cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible');
     })
 
-    it('verifica mensagens de senha com menos de 8 caracteres', () => {
+    it('verifica mensagem de senha com menos de 8 caracteres', () => {
         cy.contains('a', 'Register now').click();
         cy.contains('button', 'Register').click();
         cy.get('input[formcontrolname="password"]').type('123');
         cy.contains('button', 'Register').click();
         cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
+    })
+
+    it('verifica mensagem de letra maiúscula do user name', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="userName"]').type('AA');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Must be lower case').should('be.visible');
+    })
+
+    it('verifica mensagem de mínimo caracteres do user name', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="userName"]').type('a');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Mininum length is 2').should('be.visible');
+    })
+
+    it('verifica mensagens múltiplas do user name incorreto', () => {
+        cy.contains('a', 'Register now').click();
+        cy.contains('button', 'Register').click();
+        cy.get('input[formcontrolname="userName"]').type('A');
+        cy.contains('button', 'Register').click();
+        cy.contains('ap-vmessage', 'Mininum length is 2').should('be.visible');
+        cy.contains('ap-vmessage', 'Must be lower case').should('be.visible');
     })
 
 })
