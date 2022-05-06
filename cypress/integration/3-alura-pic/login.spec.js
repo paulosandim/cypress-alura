e038f70dab6435b2ad7d7a667de56338e12bef3f
paulo sandim <pauloedusandim@gmail.com>
2022-05-06 19:39:29 -0300

    add stub test and finish

--- cypress/integration/3-alura-pic/login.spec.js
+++ cypress/integration/3-alura-pic/login.spec.js
@@ -2,10 +2,15 @@ describe('Login de usuários alura pic', () => {
 
     beforeEach(() => {
         cy.visit('/');
+
+        cy.intercept('POST', 'https://apialurapic/user/login', {
+            statusCode: 400
+        }).as('stubPost')
     })
 
     it('fazer login de usuario valido', () => {
         cy.login(Cypress.env('userName'), Cypress.env('password'));
+        cy.wait('@stubPost')
         cy.contains('a', '(Logout)').should('be.visible');
     })
 
