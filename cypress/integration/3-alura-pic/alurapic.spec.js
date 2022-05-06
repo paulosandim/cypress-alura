c668d2adbfadde1ee8d5c95a49f75b43c05f91db
paulo sandim <pauloedusandim@gmail.com>
2022-05-06 13:07:18 -0300

    testing request api

--- cypress/integration/3-alura-pic/alurapic.spec.js
+++ cypress/integration/3-alura-pic/alurapic.spec.js
@@ -7,15 +7,15 @@ describe('Login e registro de usuários alura pic', () => {
     const usuarios = require('../../fixtures/usuarios.json');
     usuarios.forEach(usuario => {
 
-    it.only(`registra ${usuario.userName} `, () => {
-        cy.contains('a', 'Register now').click();
-        cy.contains('button', 'Register').click();
-        cy.get('input[formcontrolname="email"]').type(usuario.email);
-        cy.get('input[formcontrolname="fullName"]').type(usuario.fullName);
-        cy.get('input[formcontrolname="userName"]').type(usuario.userName);
-        cy.get('input[formcontrolname="password"]').type(usuario.password);
-        cy.contains('button', 'Register').click();
-    })
+        it.only(`registra ${usuario.userName} `, () => {
+            cy.contains('a', 'Register now').click();
+            cy.contains('button', 'Register').click();
+            cy.get('input[formcontrolname="email"]').type(usuario.email);
+            cy.get('input[formcontrolname="fullName"]').type(usuario.fullName);
+            cy.get('input[formcontrolname="userName"]').type(usuario.userName);
+            cy.get('input[formcontrolname="password"]').type(usuario.password);
+            cy.contains('button', 'Register').click();
+        })
 
 })
 
